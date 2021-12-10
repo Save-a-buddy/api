ce2c3ab448d37ead277cb4aaacc9903f158d8a65
Andrés Rivas <andreselias2303@gmail.com>
2021-12-10 17:19:19 -0300

    Auth with jwt

---
+++ internal/auth/token.go
@@ -0,0 +1,49 @@
+package auth
+
+import (
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"save-a-buddy-api/model"
+	"time"
+)
+
+func GenerateToken(data *model.Login) (string, error) {
+	claim := model.Claim{
+		Email: data.Email,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
+			Issuer:    "AE",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claim)
+	signedToken, err := token.SignedString("key_to_sign")
+
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
+
+func ValidateToken(t string) (model.Claim, error) {
+	token, err := jwt.ParseWithClaims(t, &model.Claim{}, verifyFunction)
+	if err != nil {
+		return model.Claim{}, err
+	}
+
+	if !token.Valid {
+		return model.Claim{}, errors.New("invalid token")
+	}
+
+	claim, ok := token.Claims.(*model.Claim)
+	if !ok {
+		return model.Claim{}, errors.New("error with claims")
+	}
+
+	return *claim, nil
+}
+
+func verifyFunction(t *jwt.Token) (interface{}, error) {
+	return "key_to_sign", nil
+}
