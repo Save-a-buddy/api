1ba03051cb412b6661badea0d7f17f2a2ac85e1b
Andrés Rivas <andreselias2303@gmail.com>
2021-12-11 10:02:34 -0300

    Add viper

--- cmd/main.go
+++ cmd/main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"os"
+	"save-a-buddy-api/config"
 	"save-a-buddy-api/internal/auth"
 	"save-a-buddy-api/model"
+	"save-a-buddy-api/pkg/utils"
 )
 
 func main() {
 	e := echo.New()
 
-	err := auth.LoadFiles("certificates/app.rsa", "certificates/app.rsa.pub")
+	configPath := utils.GetConfigPath(os.Getenv("config"))
+
+	cfgFile, err := config.LoadConfig(configPath)
+	if err != nil {
+		e.Logger.Fatalf("LoadConfig: %v", err)
+	}
+
+	_, err = config.ParseConfig(cfgFile)
+	if err != nil {
+		e.Logger.Fatalf("ParseConfig: %v", err)
+	}
+
+	err = auth.LoadFiles("certificates/app.rsa", "certificates/app.rsa.pub")
 	if err != nil {
 		e.Logger.Fatalf("Can't be loaded certificates: %v", err)
 	}
